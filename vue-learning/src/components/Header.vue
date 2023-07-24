85bcbc94797d26884a9d4748c6a89a89b583ff6c
hafidalazhar25 <21221753116@smkassalaambandung.sch.id>
2023-07-24 14:45:15 +0700

    p

---
+++ vue-learning/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <div>
+            
+            <h1>Ini Dari Header</h1>
+            <p>Halo {{ text }} ini dari Header</p>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: ['text'],
+}
+</script>
---
+++ vue-learning/src/components/NewComponent.vue
@@ -0,0 +1,14 @@
+<template>
+<p>Ini Dari NewComponent</p>
+<MyComponent nama="Hafid" umur="12"/>
+</template>
+
+<script>
+import MyComponent from '../components/MyComponent.vue'
+
+export default {
+    components: {
+        MyComponent
+    }
+}
+</script>
